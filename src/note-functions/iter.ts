ad00dc8290e845479eeb7bc23a8ebb5d2dc05e1b
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-11-26 17:28:17 +0900

    Fix iter

--- src/note-functions/iter.ts
+++ src/note-functions/iter.ts
@@ -7,7 +7,7 @@ export class Iter {
   ): void {
     if (top === undefined) return;
     if (top.left) Iter.iter(top.left, mutableResult);
-    if (top.right) Iter.iter(top.right, mutableResult);
     mutableResult.push([top.key, top.value]);
+    if (top.right) Iter.iter(top.right, mutableResult);
   }
 }
